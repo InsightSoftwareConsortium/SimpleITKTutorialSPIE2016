98b17a4b0201f492a9604e1f34ccc3814b0d97e4
Matt McCormick <matt.mccormick@kitware.com>
2016-01-26 01:19:24 -0500

    Add Setup notebook.

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
+./downloaddata.py ./Data ./Data/manifest.json
 docker build -t insighttoolkit/simpleitk-notebooks:2016-spie .
 docker save -o 2016-SPIE-MI-ITK-Course-Notebooks.tar insighttoolkit/simpleitk-notebooks:2016-spie
